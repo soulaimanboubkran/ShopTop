c31c2630c064806971e9251cc643d122f83af3af
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2023-11-27 22:29:42 +0100

    setup the starter files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import './app.scss'
+const Layout=()=>{
+  return(
+    <div className="app">
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
 function App() {
-  return <div>Hello World!</div>;
+const router= createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+    
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/products/:id",
+          element: <Products />,
+        },
+        {
+          path: "/product/:id",
+          element: <Product />,
+        },
+      
+    ]
+  }
+])
+  return <div>
+        <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
